4805c4044b84792973d1dbeb12324abf9d7d856e
Anthony Miguez <anm501@stern.nyu.edu>
2020-05-09 23:22:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true    
 end
